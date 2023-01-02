87b1d3831cbd80c93e3c2c2090a30481c2c7d427
Jessie Gagnon <kaiablackthorne@gmail.com>
2023-01-01 22:59:58 -0600

    Basic app functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [entries, setEntries] = useState([])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const form = event.target
+    const weight = form.elements.weight.value
+    const date = form.elements.date.value
+
+    setEntries([...entries, { weight, date}])
+    form.reset()
+  }
+
+  function handleDelete(index) {
+    const newEntries = [...entries]
+    newEntries.splice(index, 1)
+    setEntries(newEntries)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weight Tracker</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='date'>Date:</label>
+          <input 
+            type='date' 
+            name='date' 
+            id='date' 
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='weight'>Weight:</label>
+          <input 
+            type='number' 
+            name='weight' 
+            id='weight' 
+            min='0' 
+            required
+          />
+        </div>
+
+        <button type='submit'>Add Entry</button>
+      </form>
+
+      <h2>Past Entries:</h2>
+      <ul>
+        {entries.map((entry, index) => (
+          <li key={index}>
+            {entry.weight} lbs on {entry.date} 
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
